c2bbed0f94680068fcab33e1cce1f1b6182f4ae0
ZUHOWKS <jojo.vilard38@gmail.com>
2024-11-18 01:10:56 +0100

    Implements game events

--- src/main/java/fr/zuhowks/hikabrain/listeners/GameListener.java
+++ src/main/java/fr/zuhowks/hikabrain/listeners/GameListener.java
@@ -1,8 +1,10 @@
 package fr.zuhowks.hikabrain.listeners;
 
-import fr.zuhowks.hikabrain.Hikabrain;
 import fr.zuhowks.hikabrain.game.GameStatus;
+import fr.zuhowks.hikabrain.game.HikabrainManager;
 import fr.zuhowks.hikabrain.game.Team;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -15,21 +17,21 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 
 import java.util.HashMap;
 import java.util.UUID;
 
+import static fr.zuhowks.hikabrain.Hikabrain.INSTANCE;
+import static fr.zuhowks.hikabrain.Hikabrain.prefixMessage;
+
 public class GameListener implements Listener {
-    private Hikabrain INSTANCE = Hikabrain.getINSTANCE();
 
     private HashMap<UUID, UUID> entitySource = new HashMap<>();
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        //INSTANCE.getManager().join(event.getPlayer());
-        INSTANCE.getManager().getBlue().getMembers().removeIf(member -> member.getUniqueId().equals(event.getPlayer().getUniqueId()));
-        INSTANCE.getManager().getBlue().getMembers().add(event.getPlayer());
+        System.out.println(INSTANCE.getManager().getStatus());
+        INSTANCE.getManager().join(event.getPlayer());
     }
 
     @EventHandler
@@ -47,9 +49,11 @@ public class GameListener implements Listener {
     @EventHandler
     public void allowedBlockBreak(BlockBreakEvent event) {
         if (INSTANCE.getManager().getStatus() != GameStatus.GAME_NOT_SETUP) {
-            if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
-                Material type = event.getBlock().getType();
-                event.setCancelled(type != Material.SANDSTONE);
+            if (INSTANCE.getManager().isNotFreezePlayers()) {
+                if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
+                    Material type = event.getBlock().getType();
+                    event.setCancelled(type != Material.CUT_SANDSTONE);
+                }
             }
         }
     }
@@ -57,72 +61,36 @@ public class GameListener implements Listener {
     @EventHandler
     public void allowedBlockBuild(BlockPlaceEvent event) {
         if (INSTANCE.getManager().getStatus() != GameStatus.GAME_NOT_SETUP) {
-            if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
-                Block block = event.getBlock();
-                Material type = block.getType();
-                int y = block.getY();
+            if (INSTANCE.getManager().isNotFreezePlayers()) {
+                if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
+                    Block block = event.getBlock();
+                    Material type = block.getType();
+                    int y = block.getY();
 
-                int middleY = (INSTANCE.getMap().getBlueBed().getBlockY() + INSTANCE.getMap().getRedBed().getBlockY())/2;
-                event.setCancelled(type != Material.SANDSTONE || !(middleY - 5 <= y && y <= middleY + 4));
-            }
-        }
-    }
+                    int middleY = (INSTANCE.getMap().getBlueBed().getBlockY() + INSTANCE.getMap().getRedBed().getBlockY())/2;
+                    event.setCancelled(type != Material.CUT_SANDSTONE || !(middleY - 6 <= y && y <= middleY + 4));
 
-    @EventHandler
-    public void scored(PlayerMoveEvent event) {
-        if (INSTANCE.getManager().getStatus() != GameStatus.GAME_NOT_SETUP) {
-            if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
-                Player p = event.getPlayer();
-                Team team = INSTANCE.getManager().getPlayerTeam(p);
-
-                if (team != null) {
-                    Location loc = p.getLocation();
-                    Location bedLoc;
-
-                    if (team == Team.BLUE) {
-                        bedLoc = INSTANCE.getMap().getRedBed().clone();
-                    } else {
-                        bedLoc = INSTANCE.getMap().getBlueBed().clone();
-                    }
-
-                    bedLoc.setX(bedLoc.getX()+0.5);
-                    bedLoc.setY(bedLoc.getY()+0.5);
-                    bedLoc.setZ(bedLoc.getZ()+0.5);
-
-                    System.out.println(bedLoc.distance(loc));
-
-                    if (bedLoc.distance(loc) < 0.91) {
-                        //TODO: Send to all players Title point
-                        INSTANCE.getManager().setStuffAndTeleport();
+                    if (!event.isCancelled()) {
+                        if (event.getItemInHand().getAmount() <= 1) {
+                            event.getItemInHand().setAmount(64);
+                        }
                     }
                 }
-
-
+            } else {
+                event.setCancelled(true);
             }
-
         }
     }
 
     @EventHandler
     public void setEntitySource(EntityDamageEvent event) {
-        if (event.getEntity() instanceof Player attacked && event.getDamageSource().getCausingEntity() instanceof Player damaged) {
-            this.entitySource.put(attacked.getUniqueId(), damaged.getUniqueId());
-        }
-    }
-
-    @EventHandler
-    public void playerFallen(PlayerMoveEvent event) {
-
         if (INSTANCE.getManager().getStatus() != GameStatus.GAME_NOT_SETUP) {
             if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
-                int middleY = (INSTANCE.getMap().getBlueBed().getBlockY() + INSTANCE.getMap().getRedBed().getBlockY())/2;
-                double pY = event.getPlayer().getY();
-                if (pY < middleY - 10) {
-                    UUID damager = entitySource.get(event.getPlayer().getUniqueId());
-                    if (this.entitySource.get(event.getPlayer().getUniqueId()) != null) {
-                        event.getPlayer().damage(10000, INSTANCE.getServer().getPlayer(damager));
-                    } else {
-                        event.getPlayer().damage(10000);
+                if (event.getEntity() instanceof Player attacked && event.getDamageSource().getCausingEntity() instanceof Player damaged) {
+                    this.entitySource.put(attacked.getUniqueId(), damaged.getUniqueId());
+                    if (attacked.getHealth() <= 0) {
+                        event.setCancelled(true);
+                        INSTANCE.getManager().respawnPlayer(attacked);
                     }
                 }
             }
@@ -130,12 +98,77 @@ public class GameListener implements Listener {
     }
 
     @EventHandler
-    public void playerDeath(PlayerRespawnEvent event) {
+    public void onPlayerMove(PlayerMoveEvent event) {
         if (INSTANCE.getManager().getStatus() != GameStatus.GAME_NOT_SETUP) {
-            if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
-                INSTANCE.getManager().respawnPlayer(event.getPlayer());
+            if (INSTANCE.getManager().isNotFreezePlayers()) {
+                if (INSTANCE.getManager().getStatus() == GameStatus.IN_GAME) {
+                    onFallen(event);
+                    onScore(event);
+                }
+            } else {
+                Team team = INSTANCE.getManager().getPlayerTeam(event.getPlayer().getUniqueId());
+                if (team == Team.BLUE) {
+                    HikabrainManager.teleportPlayer(event.getPlayer(), INSTANCE.getMap().getBlueSpawn(), INSTANCE.getMap().getRedSpawn());
+                }
+
+            }
+
+        }
+    }
+
+    private static void onScore(PlayerMoveEvent event) {
+        Player p = event.getPlayer();
+        Team team = INSTANCE.getManager().getPlayerTeam(p.getUniqueId());
+
+        if (team != null) {
+            Location loc = p.getLocation();
+            Location bedLoc;
+
+            if (team == Team.BLUE) {
+                bedLoc = INSTANCE.getMap().getRedBed().clone();
+            } else {
+                bedLoc = INSTANCE.getMap().getBlueBed().clone();
+            }
+
+            bedLoc.setX(bedLoc.getX()+0.5);
+            bedLoc.setY(bedLoc.getY()+0.5);
+            bedLoc.setZ(bedLoc.getZ()+0.5);
+
+            if (bedLoc.distance(loc) < 0.91) {
+                INSTANCE.getManager().teamScored(p, team);
             }
         }
     }
 
+    private void onFallen(PlayerMoveEvent event) {
+        int middleY = (INSTANCE.getMap().getBlueBed().getBlockY() + INSTANCE.getMap().getRedBed().getBlockY())/2;
+        Player p = event.getPlayer();
+        double pY = p.getY();
+        if (pY < middleY - 10) {
+            UUID damager = entitySource.get(p.getUniqueId());
+            if (this.entitySource.get(p.getUniqueId()) != null) {
+                Player d = Bukkit.getPlayer(damager);
+                if (d != null) {
+                    INSTANCE.getManager().respawnPlayer(p);
+                    Team playerTeam = INSTANCE.getManager().getPlayerTeam(p.getUniqueId());
+                    Team damagerTeam = INSTANCE.getManager().getPlayerTeam(d.getUniqueId());
+                    if (playerTeam != null && damagerTeam != null) {
+                        for (Player _p : Bukkit.getOnlinePlayers()) {
+                            _p.sendMessage(prefixMessage + playerTeam.getColor() + p.getName() + ChatColor.GOLD + " a été tué par " + damagerTeam.getColor() + d.getName() + ChatColor.GOLD + ".");
+                        }
+                    }
+                }
+
+            } else {
+                INSTANCE.getManager().respawnPlayer(p);
+                Team playerTeam = INSTANCE.getManager().getPlayerTeam(p.getUniqueId());
+                if (playerTeam != null) {
+                    for (Player _p : Bukkit.getOnlinePlayers()) {
+                        _p.sendMessage(prefixMessage + playerTeam.getColor() + p.getName() + ChatColor.GOLD + " a glissé. Désolé chef!");
+                    }
+                }
+
+            }
+        }
+    }
 }
